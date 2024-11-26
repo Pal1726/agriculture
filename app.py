be34d08f5d3a221cdf22d5f3fa5ace6e2cc41c57
Palak <palakrathiiit@gmail.com>
2024-11-26 14:38:33 +0530

    modified and added some files

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ def get_db_connection():
     )
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
 
 @app.route('/sellers')
 def sellers():
